399f361141bd556e949dfa93d52ad88968b8389a
ramos <rdiego26@gmail.com>
2019-08-03 19:55:17 -0300

    ADD sequelize and pool configuration

---
+++ src/utils/poolConnection.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const	constants = require('./constants');
+
+const poolInstance = new Sequelize(constants.db.database, constants.db.username, constants.db.password, {
+	host: constants.db.host,
+	dialect: 'postgres',
+	logging: false,
+	pool: {
+		max: 20,
+		min: 5,
+		idle: 10000
+	}
+});
+
+module.exports = poolInstance;
+
