4b24335658fa0ad48a9d5df3b2e42ac13f555cdd
ramos <rdiego26@gmail.com>
2019-08-26 19:43:45 -0300

    ADD more tests

--- test/daos/configItemDaoTest.js
+++ test/daos/configItemDaoTest.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const faker = require('faker');
 const assert = require('chai').assert;
 const dao = require(path.resolve('src/daos/configItemDao'));
 
@@ -13,9 +14,22 @@ describe('Config Item DAO', () => {
 		try {
 			await dao.create(invalidData);
 		} catch (e) {
-			console.log(`e=${e}`);
 			assert.isNotNull(e);
 		}
+
+	});
+
+	it('should create when send valid data', async() => {
+		const validData = {
+			key: faker.lorem.text(),
+			value: faker.lorem.text()
+		};
+
+		let createdItem = await dao.create(validData);
+		assert.isNotNull(createdItem);
+		assert.isObject(createdItem);
+		assert.ok(createdItem.key === validData.key);
+		assert.ok(createdItem.value === validData.value);
 
 	});
 
